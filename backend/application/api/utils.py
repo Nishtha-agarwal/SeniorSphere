a43a2d461f32cc32c42416e6e223ae63a253cfe4
Nishtha-agarwal <93310872+Nishtha-agarwal@users.noreply.github.com>
2025-09-10 13:29:06 +0530

    Delete backend/application/api/utils.py

--- backend/application/api/utils.py
+++
@@ -1,8 +0,0 @@
-from jinja2 import Template
-
-
-def format_report(html_template, data):
-    with open(html_template) as file:
-        template = Template(file.read())
-        return template.render(data=data)
-
